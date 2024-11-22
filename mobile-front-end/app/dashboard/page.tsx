18297d2b9c994f62a4152317c2e1d66f1a2d05fa
mathis sergent <sergent.mathis@gmail.com>
2024-11-22 10:19:19 +0100

    Remove login page, add page link

--- mobile-front-end/app/dashboard/page.tsx
+++ mobile-front-end/app/dashboard/page.tsx
@@ -2,13 +2,31 @@
 import Image from "next/image";
 
 // UI
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 
 export default function Dashboard() {
     return (
         <main>
             <h1>Welcome to IDEX Supply Chain App</h1>
-            <Button>Hello</Button>
+            <Card className="max-w-[400px]">
+                <CardHeader className="flex gap-3">
+                    <div className="flex flex-col">
+                        <p className="text-md">NextUI</p>
+                        <p className="text-small text-default-500">nextui.org</p>
+                    </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                    <p>Make beautiful websites regardless of your design experience.</p>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                    <Link showAnchorIcon>
+                        Visit source code on GitHub.
+                    </Link>
+                </CardFooter>
+            </Card>
         </main>
     );
 }
--- mobile-front-end/app/page.tsx
+++ mobile-front-end/app/page.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 
 // UI
 import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome to IDEX Supply Chain App</h1>
-      <Button>Start</Button>
+      <Link href="/dashboard"><Button>Start</Button></Link>
     </main>
   );
 }
