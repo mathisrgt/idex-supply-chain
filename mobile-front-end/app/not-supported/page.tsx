adf4a9485a52caf5aeb76209eadb40055cd18daf
mathis sergent <sergent.mathis@gmail.com>
2024-12-01 23:08:24 +0100

    Try again button

--- mobile-front-end/app/not-supported/page.tsx
+++ mobile-front-end/app/not-supported/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
-import { Smartphone } from "lucide-react";
+import { Button } from "@nextui-org/react";
+import { Redo2, Smartphone } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function NotSupported() {
+    const router = useRouter();
+
     return (
         <>
-            <main className="p-4 flex flex-col gap-4 justify-center min-h-screen items-center">
+            <main className="p-4 flex flex-col gap-6 justify-center min-h-screen items-center">
                 <Smartphone size={40} />
-                <h1 className="text-xl">Sorry, this device is not supported.</h1>
-                <p className="text-body">WoodTracker is only available on smartphone.</p>
+                <div className="flex flex-col gap-2 justify-center items-center">
+                    <h1 className="text-xl">Sorry, this device is not supported.</h1>
+                    <p className="text-sm text-slate-600">WoodTracker is only available on smartphone.</p>
+                </div>
+                <Button color="primary" onClick={() => {
+                    router.back();
+                }}>
+                    <Redo2 />
+                    Try again
+                </Button>
             </main>
         </>
     );
